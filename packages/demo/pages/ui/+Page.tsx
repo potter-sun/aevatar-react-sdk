247aa294be6f387573a13aed1efc4678a06f4367
potter <potter.sun@aelf.io>
2025-02-17 14:35:59 +0800

    feat: 🎸 my g-aevatar list

--- packages/demo/pages/ui/+Page.tsx
+++ packages/demo/pages/ui/+Page.tsx
@@ -1,12 +1,16 @@
 import { MyGAevatar } from "@aevatar-react-sdk/ui-react";
 import "@aevatar-react-sdk/ui-react/ui-react.css";
+import { useCallback } from "react";
 
 export default function UI() {
+  const onNewGAevatar = useCallback(() => {
+    console.log("onNewGAevatar");
+  }, []);
   return (
     <div>
       <div className="text-[12px] lg:text-[24px]">aad</div>
-      
-      <MyGAevatar height={600} />
+
+      <MyGAevatar height={600} onNewGAevatar={onNewGAevatar} />
     </div>
   );
 }
